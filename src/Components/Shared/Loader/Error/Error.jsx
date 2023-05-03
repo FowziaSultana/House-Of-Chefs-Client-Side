22caf16f6627efe09a8a13212b42562dc2b24a58
FowziaSultana <fowziasultana168@gmail.com>
2023-05-03 22:08:58 +0600

    updated error page

--- src/Components/Shared/Loader/Error/Error.jsx
+++ src/Components/Shared/Loader/Error/Error.jsx
@@ -4,7 +4,7 @@ import page from "../../../../assets/image/CodePen-404-Page.png";
 const Error = () => {
   return (
     <div className="error">
-      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+      <div className="bg-indigo-800 relative overflow-hidden h-screen">
         <img src={page} className="absolute h-full w-full object-cover" />   {" "}
         <div className="inset-0 bg-black opacity-25 absolute">    </div>   {" "}
         <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
