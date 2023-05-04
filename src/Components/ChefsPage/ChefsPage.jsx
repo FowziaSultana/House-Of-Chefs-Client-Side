70cc62ea12a0532bb1d6cd9290ed592c736479d8
FowziaSultana <fowziasultana168@gmail.com>
2023-05-04 22:06:43 +0600

    testing git push problem

--- src/Components/ChefsPage/ChefsPage.jsx
+++ src/Components/ChefsPage/ChefsPage.jsx
@@ -31,18 +31,18 @@ const ChefsPage = () => {
       });
   }, [ChefId]);
 
-  if (load) {
-    return (
-      <div
-        className=" inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
-    );
-  }
+  // if (load) {
+  //   return (
+  //     <div
+  //       className=" inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+  //       role="status"
+  //     >
+  //       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+  //         Loading...
+  //       </span>
+  //     </div>
+  //   );
+  // }
 
   //private route for single chefs details view
   return (
